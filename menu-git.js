3fd2d50ca3480e647410e5d07a56480f7fa5b79e
borisgra <boris.gra.dev@gmail.com>
2025-03-24 20:43:44 +0000

    Soundproofing

--- menu-git.js
+++ menu-git.js
@@ -24,7 +24,7 @@ export function menuItemsData(newGreed)  {
                 sx: defColor
             },
             {
-                label: 'Soundproofing)',
+                label: 'Soundproofing',
                 sx: defColor,
                 delay: 300,
                 items: [
